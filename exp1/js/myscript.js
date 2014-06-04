2bb9281dbd9d40e3a5f302ca426bea66f092a299
ghanavel <jerinavel@gmail.com>
2014-06-04 12:11:03 +0530

    eddit 3 (own)

--- exp1/js/myscript.js
+++ exp1/js/myscript.js
@@ -93,6 +93,55 @@ console.log(ageGroup["30"]); // Children
 
 
 
+// Reference Data Type and Primitive Data Types
+
+/*
+One of the main differences between reference data type and primitive data types 
+is reference data type’s value is stored as a reference, it is not stored directly on the variable, 
+as a value, as the primitive data types are. For example:
+*/
+
+
+// The primitive data type String is stored as a value
+var person = "Kobe";  
+var anotherPerson = person; // anotherPerson = the value of person
+person = "Bryant"; // value of person changed
+
+console.log(anotherPerson); // Kobe
+console.log(person); // Bryant
+
+/*
+It is worth noting that even though we changed person to “Bryant,” the anotherPerson variable still 
+retains the value that person had.
+*/
+
+
+// Compare the primitive data saved-as-value demonstrated above with the save-as-reference for objects:
+var person = {name: "Kobe"};
+var anotherPerson = person;
+person.name = "Bryant";
+
+console.log(anotherPerson.name); // Bryant
+console.log(person.name); // Bryant
+
+/*
+In this example, we copied the person object to anotherPerson, but because the value in person was stored as
+ a reference and not an actual value, when we changed the person.name property to “Bryant” the anotherPerson 
+reflected the change because it never stored an actual copy of it’s own value of the person’s properties, 
+it only had a reference to it.
+*/
+
+
+/*
+Object Data Properties Have Attributes
+Each data property (object property that store data) has not only the name-value pair, but also 3 attributes (the three attributes are set to true by default):
+— Configurable Attribute: Specifies whether the property can be deleted or changed.
+— Enumerable: Specifies whether the property can be returned in a for/in loop.
+— Writable: Specifies whether the property can be changed.
+
+
+*/
+
 
 (function test() { 
 	var test = 123; 
