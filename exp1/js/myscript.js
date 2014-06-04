fc53576d7717f9143c1762e51561aa16e78565af
ghanavel <jerinavel@gmail.com>
2014-06-04 13:22:51 +0530

    edit 5

--- exp1/js/myscript.js
+++ exp1/js/myscript.js
@@ -138,10 +138,307 @@ Each data property (object property that store data) has not only the name-value
 — Configurable Attribute: Specifies whether the property can be deleted or changed.
 — Enumerable: Specifies whether the property can be returned in a for/in loop.
 — Writable: Specifies whether the property can be changed.
+*/
 
+
+/*
+
+Creating Objects
+These are the two common ways to create objects:
+
+1. Object Literals
+The most common and, indeed, the easiest way to create objects is with the object literal described here
+
+*/
+// This is an empty object initialized using the object literal notation
+var myBooks = {};
+
+// This is an object with 4 items, again using object literal
+var mango = {
+color: "yellow",
+shape: "round",
+sweetness: 8,
+
+howSweetAmI: function () {
+console.log("Hmm Hmm Good");
+}
+}
+
+/*
+2. Object Constructor
+  The second most common way to create objects is with Object constructor. 
+  A constructor is a function used for initializing new objects, and you use the new keyword to call the constructor.
+*/
+
+
+var mango =  new Object ();
+mango.color = "yellow";
+mango.shape= "round";
+mango.sweetness = 8;
+
+mango.howSweetAmI = function () {
+console.log("Hmm Hmm Good");
+}
+
+
+
+
+
+1. Constructor Pattern for Creating Objects
+function Fruit (theColor, theSweetness, theFruitName, theNativeToLand) {
+
+    this.color = theColor;
+    this.sweetness = theSweetness;
+    this.fruitName = theFruitName;
+    this.nativeToLand = theNativeToLand;
+
+    this.showName = function () {
+        console.log("This is a " + this.fruitName);
+    }
+
+    this.nativeTo = function () {
+    this.nativeToLand.forEach(function (eachCountry)  {
+       console.log("Grown in:" + eachCountry);
+        });
+    }
+
+
+}
+
+var mangoFruit = new Fruit ("Yellow", 8, "Mango", ["South America", "Central America", "West Africa"]);
+mangoFruit.showName(); // This is a Mango.
+mangoFruit.nativeTo();
+//Grown in:South America
+// Grown in:Central America
+// Grown in:West Africa
+
+var pineappleFruit = new Fruit ("Brown", 5, "Pineapple", ["United States"]);
+pineappleFruit.showName(); // This is a Pineapple.
+
+
+
+
+// 2. Prototype Pattern for Creating Objects
+function Fruit () {
+
+}
+
+Fruit.prototype.color = "Yellow";
+Fruit.prototype.sweetness = 7;
+Fruit.prototype.fruitName = "Generic Fruit";
+Fruit.prototype.nativeToLand = "USA";
+
+Fruit.prototype.showName = function () {
+console.log("This is a " + this.fruitName);
+}
+
+Fruit.prototype.nativeTo = function () {
+            console.log("Grown in:" + this.nativeToLand);
+}
+
+
+/*
+
+How to Access Properties on an Object
+The two primary ways of accessing properties of an object are with dot notation and bracket notation.
+*/
+// 1. Dot Notation
+// We have been using dot notation so far in the examples above, here is another example again:
+var book = {title: "Ways to Go", pages: 280, bookMark1:"Page 20"};
+
+// To access the properties of the book object with dot notation, you do this:
+console.log ( book.title); // Ways to Go
+console.log ( book.pages); // 280
+
+// 2. Bracket Notation
+
+// To access the properties of the book object with bracket notation, you do this:
+console.log ( book["title"]); //Ways to Go
+console.log ( book["pages"]); // 280
+
+//Or, in case you have the property name in a variable:
+var bookTitle = "title";
+console.log ( book[bookTitle]); // Ways to Go
+console.log (book["bookMark" + 1]); // Page 20
+
+// Own and Inherited Properties
+// Create a new school object with a property name schoolName
+var school = {schoolName:"MIT"};
+
+// Prints true because schoolName is an own property on the school object
+console.log("schoolName" in school);  // true
+
+// Prints false because we did not define a schoolType property on the school object, and neither did the object inherit a schoolType property from its prototype object Object.prototype.
+console.log("schoolType" in school);  // false
+ 
+// Prints true because the school object inherited the toString method from Object.prototype. 
+console.log("toString" in school);  // true
+
+/*
+hasOwnProperty
+To find out if an object has a specific property as one of its own property, you use the hasOwnProperty method.
+This method is very useful because from time to time you need to enumerate an object and you want only the own properties, not the inherited ones.
+*/
+// Create a new school object with a property name schoolName
+var school = {schoolName:"MIT"};
+
+// Prints true because schoolName is an own property on the school object
+console.log(school.hasOwnProperty ("schoolName"));  // true
+ 
+// Prints false because the school object inherited the toString method from Object.prototype, therefore toString is not an own property of the school object.
+console.log(school.hasOwnProperty ("toString"));  // false 
+
+
+/*
+Accessing and Enumerating Properties on Objects
+To access the enumerable (own and inherited) properties on objects, you use the for/in loop or a general for loop.
+*/
+
+// Create a new school object with 3 own properties: schoolName, schoolAccredited, and schoolLocation.
+var school = {schoolName:"MIT", schoolAccredited: true, schoolLocation:"Massachusetts"};
+
+//Use of the for/in loop to access the properties in the school object
+for (var eachItem in school) {
+console.log(eachItem); // Prints schoolName, schoolAccredited, schoolLocation
+
+}
+
+/*
+
+Accessing Inherited Properties
+Properties inherited from Object.prototype are not enumerable, so the for/in loop does not show them. 
+However, inherited properties that are enumerable are revealed in the for/in loop iteration.
+For example:
 
 */
 
+
+ //Use of the for/in loop to access the properties in the school object
+for (var eachItem in school) {
+console.log(eachItem); // Prints schoolName, schoolAccredited, schoolLocation
+
+}
+
+// Create a new HigherLearning function that the school object will inherit from.
+/* SIDE NOTE: As Wilson (an astute reader) correctly pointed out in the comments below, the educationLevel property is not actually inherited by objects that use the HigherLearning constructor; instead, the educationLevel property is created as a new property on each object that uses the HigherLearning constructor. The reason the property is not inherited is because we use of the "this" keyword to define the property.
+*/
+
+
+function HigherLearning () {
+this.educationLevel = "University";
+}
+
+// Implement inheritance with the HigherLearning constructor
+var school = new HigherLearning ();
+school.schoolName = "MIT";
+school.schoolAccredited = true;
+school.schoolLocation = "Massachusetts";
+
+
+//Use of the for/in loop to access the properties in the school object
+for (var eachItem in school) {
+console.log(eachItem); // Prints educationLevel, schoolName, schoolAccredited, and schoolLocation
+}
+
+
+/*
+In the last example, note the educationLevel property that was defined on the HigherLearning function is listed as
+one of the school’s properties, even though educationLevel is not an own property—it was inherited.
+*/
+
+
+/*
+
+Deleting Properties of an Object:
+To delete a property from an object, you use the delete operator. 
+You cannot delete properties that were inherited, nor can you delete properties with their attributes set to configurable.
+You must delete the inherited properties on the prototype object (where the properties were defined).
+
+Also, you cannot delete properties of the global object, which were declared with the var keyword.
+
+The delete operator returns true if the delete was successful. And surprisingly, it also returns true if the property to delete was nonexistent or the property could not be deleted (such as non-configurable or not owned by the object).
+
+These examples illustrate:
+
+*/
+
+var christmasList = {mike:"Book", jason:"sweater" }
+delete christmasList.mike; // deletes the mike property
+
+for (var people in christmasList) {
+	console.log(people);
+}
+// Prints only jason
+// The mike property was deleted
+
+delete christmasList.toString; // returns true, but toString not deleted because it is an inherited method
+
+// Here we call the toString method and it works just fine—wasn’t deleted 
+christmasList.toString(); //"[object Object]"
+
+// You can delete a property of an instance if the property is an own property of that instance. For example, we can delete the educationLevel property from the school's object we created above because the educationLevel property is defined on the instance: we used the "this" keyword to define the property when we declare the HigherLearning function. We did not define the educationLevel property on the HigherLearning function's prototype.
+
+console.log(school.hasOwnProperty("educationLevel")); true
+// educationLevel is an own property on school, so we can delete it
+delete school.educationLevel; true 
+
+// The educationLevel property was deleted from the school instance
+console.log(school.educationLevel); undefined
+
+// But the educationLevel property is still on the HigherLearning function
+var newSchool = new HigherLearning ();
+console.log(newSchool.educationLevel); // University
+
+// If we had defined a property on the HigherLearning function's prototype, such as this educationLevel2 property:
+HigherLearning.prototype.educationLevel2 = "University 2";
+
+// Then the educationLevel2 property on the instances of HigherLearning would not be own property. 
+
+// The educationLevel2 property is not an own property on the school instance
+console.log(school.hasOwnProperty("educationLevel2")); false
+console.log(school.educationLevel2); // University 2
+
+// Let's try to delete the inherited educationLevel2 property
+delete school.educationLevel2; true (always returns true, as noted earlier)
+
+// The inherited educationLevel2 property was not deleted
+console.log(school.educationLevel2); University 2
+
+/*
+Serialize and Deserialize Objects
+To transfer your objects via HTTP or to otherwise convert it to a string, you will need to serialize it (convert it to a string); you can use the 
+JSON.stringify function to serialize your objects. Note that prior to ECMAScript 5, you had to use a popular
+ json2 library (by Douglas Crockford) to get the JSON.stringify function. It is now standardized in ECMAScript 5.
+
+To Deserialize your object (convert it to an object from a string), you use the JSON.parse function from the same json2 
+library. 
+This function too has been standardized by ECMAScript 5.
+*/
+
+// JSON.stringify Examples:
+
+var christmasList = {mike:"Book", jason:"sweater", chelsea:"iPad" }
+JSON.stringify (christmasList);
+// Prints this string:
+// "{"mike":"Book","jason":"sweater","chels":"iPad"}"
+
+// To print a stringified object with formatting, add "null" and "4" as parameters:
+JSON.stringify (christmasList, null, 4);
+// "{
+    "mike": "Book",
+    "jason": "sweater",
+    "chels": "iPad"
+}"
+
+// JSON.parse Examples 
+// The following is a JSON string, so we cannot access the properties with dot notation (like christmasListStr.mike)
+var christmasListStr = '{"mike":"Book","jason":"sweater","chels":"iPad"}';
+
+// Let’s convert it to an object
+var christmasListObj = JSON.parse (christmasListStr); 
+
+// Now that it is an object, we use dot notation
+console.log(christmasListObj.mike); // Book
 
 (function test() { 
 	var test = 123; 
@@ -305,9 +602,5 @@ console.log(typeof myName); // string
 var myName = function () {
 console.log ("Rich");
 }
-
-
-
-
 
 -------------------------------------------------------------------------------------------------------------------------
